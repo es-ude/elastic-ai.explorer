f68a1dd27f43583e8589c50f686443e2652911cc
Christopher Ringhofer <christopher.ringhofer@uni-due.de>
2024-11-08 12:00:55 +0100

    Added choices for MLP

--- elasticai/explorer/search_space.py
+++ elasticai/explorer/search_space.py
@@ -6,20 +6,23 @@ from nni.nas.nn.pytorch import LayerChoice, ModelSpace
 from mutable_types import MutableDropout, MutableLinear
 
 
-class MLP(ModelSpace):  # should inherit ModelSpace rather than nn.Module
+class MLP(ModelSpace):
     def __init__(self):
         super().__init__()
-        self.conv1 = nn.Conv2d(1, 32, 3, 1)
-        self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.dropout1 = nn.Dropout(0.25)
-        self.dropout2 = nn.Dropout(0.5)
-        self.fc1 = nn.Linear(9216, 128)
-        self.fc2 = nn.Linear(128, 10)
+        h1 = 512
+        h2 = 512
+        h1 = nni.choice("h1", [256, 512])
+        h2 = nni.choice("h2", [128, 512])
+        self.fc1 = MutableLinear(28 * 28, h1)
+        self.fc2 = MutableLinear(h1, h2)
+        self.fc3 = MutableLinear(h2, 10)
+        self.dropout = nn.Dropout(0.2)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(self.conv2(x), 2)
-        x = torch.flatten(self.dropout1(x), 1)
-        x = self.fc2(self.dropout2(F.relu(self.fc1(x))))
-        output = F.log_softmax(x, dim=1)
-        return output
+        x = x.view(-1, 28 * 28)
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = F.relu(self.fc2(x))
+        x = self.dropout(x)
+        x = self.fc3(x)
+        return x
