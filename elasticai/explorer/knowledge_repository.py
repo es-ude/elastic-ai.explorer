c3195374885c701df3b55a3110bf453c6ef1c48d
Natalie Maman <natalie.maman@uni-due.de>
2024-12-12 17:47:16 +0100

    feat(knowledge_repo): add basic info for hw_platforms. Restructure explorer and nas

---
+++ elasticai/explorer/knowledge_repository.py
@@ -0,0 +1,25 @@
+from dataclasses import dataclass
+from typing import Type
+
+from elasticai.generator.generator import Generator, PIGenerator
+
+
+@dataclass
+class HWPlatform:
+    name: str
+    info: str
+    model_generator: Type[Generator]
+
+class KnowledgeRepository:
+    def __init__(self):
+        self.supported_hw_platforms={}
+        self.register_hw_platform(HWPlatform("rpi5", "Raspberry PI 5 with A76 processor and 8GB RAM", PIGenerator))
+
+    def register_hw_platform(self, platform:HWPlatform):
+        self.supported_hw_platforms[platform.name]=platform
+
+
+
+    def fetch_hw_info(self, name: str)-> HWPlatform:
+        return self.supported_hw_platforms[name]
+
--- mlp_for_pi.py
+++ mlp_for_pi.py
@@ -1,60 +1,21 @@
-import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision.datasets import MNIST
-from torchvision.transforms import transforms
+from elasticai.explorer.explorer import Explorer
+from elasticai.explorer.knowledge_repository import KnowledgeRepository
+from elasticai.explorer.train_model import train, test
 
-from elasticai.explorer import explorer
-from elasticai.generator.generator import PIGenerator
 
-def test(model, test_loader):
-    test_loss = 0
-    correct = 0
-    model.eval()
-    with torch.no_grad():
-        for data, target in test_loader:
-            output = model(data)
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
-    test_loss /= len(test_loader.dataset)
-    accuracy = 100. * correct / len(test_loader.dataset)
-    print('\nTest set: Accuracy: {}/{} ({:.0f}%)\n'.format(
-        correct, len(test_loader.dataset), accuracy))
-    return accuracy
-
-def train_epoch(model: torch.nn.Module, device, train_loader: DataLoader, optimizer, epoch):
-    loss_fn = nn.CrossEntropyLoss()
-    model.train(True)
-    for batch_idx, (data, target) in enumerate(train_loader):
-        data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        loss = loss_fn(output, target)
-        loss.backward()
-        optimizer.step()
-        if batch_idx % 10 == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                        100. * batch_idx / len(train_loader), loss.item()))
-
-def train(model: torch.nn.Module):
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
-    model.to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    transf = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
-    train_loader = DataLoader(MNIST("data/mnist", download=True, transform=transf), batch_size=64, shuffle=True)
-    for epoch in range(5):
-        train_epoch(model, device, train_loader, optimizer, epoch)
-
-if __name__ == '__main__':
-    transf = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
+def find_and_generate_for_pi():
+    knowledge_repository= KnowledgeRepository()
+    explorer= Explorer(knowledge_repository)
+    explorer.choose_target_hw("rpi5")
     top_models=explorer.search()
-    test_loader = DataLoader(MNIST("data/mnist", download=True, train=False, transform=transf), batch_size=64)
     for i, top_model in enumerate(top_models):
         train(top_model)
-        test(top_model, test_loader)
-        generator= PIGenerator()
-        ts_model= generator.generate(top_model, path="models/ts_models/model " +str(i))
-        test(ts_model, test_loader)
-        data, target= next(iter(test_loader))
-        torch.testing.assert_close(top_model(data), ts_model(data))
+        test(top_model)
+        ts_model=explorer.generate_for_hw_platform(top_model, path="models/ts_models/model " +str(i) )
+        test(ts_model)
+
+
+
+if __name__ == '__main__':
+    find_and_generate_for_pi()
+
