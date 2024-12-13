4f358d704094de45ef739955abe5e071cf9030c8
Natalie Maman <natalie.maman@uni-due.de>
2024-12-13 11:02:27 +0100

    refactor(knowledge_repo): reformat

--- elasticai/explorer/knowledge_repository.py
+++ elasticai/explorer/knowledge_repository.py
@@ -10,16 +10,14 @@ class HWPlatform:
     info: str
     model_generator: Type[Generator]
 
+
 class KnowledgeRepository:
     def __init__(self):
-        self.supported_hw_platforms={}
+        self.supported_hw_platforms = {}
         self.register_hw_platform(HWPlatform("rpi5", "Raspberry PI 5 with A76 processor and 8GB RAM", PIGenerator))
 
-    def register_hw_platform(self, platform:HWPlatform):
-        self.supported_hw_platforms[platform.name]=platform
+    def register_hw_platform(self, platform: HWPlatform):
+        self.supported_hw_platforms[platform.name] = platform
 
-
-
-    def fetch_hw_info(self, name: str)-> HWPlatform:
+    def fetch_hw_info(self, name: str) -> HWPlatform:
         return self.supported_hw_platforms[name]
-
