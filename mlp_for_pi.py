d49207a44e5b51153c5fdaec5667f46b4052662e
Natalie Maman <natalie.maman@uni-due.de>
2025-01-09 10:18:38 +0100

    feat(latency-measurements): fix pullrequest notes and some names

--- mlp_for_pi.py
+++ mlp_for_pi.py
@@ -8,50 +8,51 @@ from settings import ROOT_DIR
 
 def setup_knowledge_repository():
     knowledge_repository = KnowledgeRepository()
-    device = ConnectionData("transpi5.local", "ies")
     knowledge_repository.register_hw_platform(
         HWPlatform(
             "rpi5",
             "Raspberry PI 5 with A76 processor and 8GB RAM",
             PIGenerator,
             PIHWManager,
-            device,
         )
     )
     return knowledge_repository
 
 
-def find_and_generate_for_pi(knowledge_repository):
+def find_and_generate_for_pi(knowledge_repository, device_connection):
     explorer = Explorer(knowledge_repository)
     explorer.choose_target_hw("rpi5")
     explorer.generate_search_space()
     top_models = explorer.search()
     measurements = []
-    for i, top_model in enumerate(top_models):
-        train(top_model)
-        test(top_model)
+    for i, model in enumerate(top_models):
+        train(model)
+        test(model)
         model_path = str(ROOT_DIR) + "/models/ts_models/model_" + str(i)
-        ts_model = explorer.generate_for_hw_platform(top_model, model_path)
-        measurements.append(explorer.run_measurement(model_path + ".pt"))
+        ts_model = explorer.generate_for_hw_platform(model, model_path)
+        measurements.append(
+            explorer.run_measurement(device_connection, model_path + ".pt")
+        )
 
     print(measurements)
 
 
-def take_measurements(knowledge_repository):
+def take_measurements(knowledge_repository, connection_data):
     explorer = Explorer(knowledge_repository)
     explorer.choose_target_hw("rpi5")
     measurements = []
     model_path = str(ROOT_DIR) + "/models/ts_models/model_0.pt"
     for i in range(20):
-        measurements.append(explorer.run_measurement(model_path))
+        measurements.append(explorer.run_measurement(connection_data, model_path))
     print(measurements)
 
 
-def prepare_and_take_measurements():
-    hw_manager = PIHWManager(ConnectionData("transpi5.local", "ies"))
+def prepare_pi():
+    hw_manager = PIHWManager()
     hw_manager.compile_code()
 
 
 if __name__ == "__main__":
     knowledge_repo = setup_knowledge_repository()
-    find_and_generate_for_pi(knowledge_repo)
+    device_connection = ConnectionData("transpi5.local", "ies")
+    find_and_generate_for_pi(knowledge_repo, device_connection)
