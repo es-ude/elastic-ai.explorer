7538419716234187f919f9e2408c0c3fd1129624
RobinFeldmann <robinfeld.ued@gmail.com>
2025-01-14 14:11:08 +0100

    Feat: Added Visualizer Prototyp and test script

--- elasticai/explorer/visualizer.py
+++ elasticai/explorer/visualizer.py
@@ -1,24 +1,44 @@
 from elasticai.explorer.knowledge_repository import KnowledgeRepository
 import matplotlib.pyplot as plt
 import numpy as np
+from settings import ROOT_DIR
 
+CONTEXT_PATH = ROOT_DIR / "plots"
 class Visualizer:
 
-    def __init__(self):
-        pass
+    def __init__(self, data):
+        self.data = np.array(data)
 
-    def plot_all_results(self, data, figure_size = [15, 20]):
+    def plot_all_results(self, figure_size = [15, 20], filename = None):
         plt.figure()
         fig = plt.figure(num=1, clear=True)
         fig.set_size_inches(figure_size[0], figure_size[1], forward=True)
     
-        ax1=fig.add_subplot(111)
-        ax2=fig.add_subplot(212)
+        ax1=fig.add_subplot(311)
+        ax2=fig.add_subplot(312)
         ax3=fig.add_subplot(313)
+        bar_width = 0.35
+        
+        indices = np.arange(0, len(self.data[0][0][:]), 1)
 
-        np_data= np.array(data)
-        indices = np.arange(0, len(data[:][0]), 1)
+        #Accuracy Estimate vs Accuracy on Pi
+        ax1.bar(x=indices, height = self.data[0][0][:], width = bar_width,label= "Accuracy in %")
+        ax1.bar(x=indices+bar_width, height = self.data[0][1][:], width = bar_width,label= "Accuracyasdasda in %")
 
-        ax1.scatter(x=indices, y = np_data[:][0], s=10, label= "Accuracy in %")
-        ax2.scatter(x=indices, y = np_data[:][1], s=10, label= "Latenzy in Mircosec.")
-        ax3.scatter(x=indices, y = np_data[:][2], s=10, label= "Combined Metric")
+        #FLOPS Proxy vs Latency on Pi
+        ax2.bar(x=indices, height = self.data[1][0][:], width = bar_width,label= "Latenzy in Mircosec.")
+        ax2.bar(x=indices+bar_width, height = self.data[1][1][:], width = bar_width,label= "FLOPs Estimation log(10)")
+
+        #Combined Metric, accuracy-estimation and flops estimation
+        ax3.bar(x=indices, height = self.data[2][0][:], width = bar_width, label= "Combined Metric")
+        ax3.bar(x=indices+bar_width, height = self.data[2][1][:], width = bar_width,label= "Accuracy")
+
+
+        ax1.legend(loc = "upper left")
+        ax2.legend(loc = "upper left")
+        ax3.legend(loc = "upper left")
+        
+        if filename:
+            plt.savefig(str(CONTEXT_PATH) + "/" + filename + ".png")
+        else:
+            plt.show()
