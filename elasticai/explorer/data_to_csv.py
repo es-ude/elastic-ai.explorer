051759f607b7d848c1fe14502568cb10e93c9180
Natalie Maman <natalie.maman@uni-due.de>
2025-01-17 15:57:43 +0100

    feat: add data to csv and parallel_coordinates_plot

---
+++ elasticai/explorer/data_to_csv.py
@@ -0,0 +1,50 @@
+import json
+
+import pandas
+import plotly.express as px
+
+from settings import ROOT_DIR
+
+
+def build_search_space_measurements_file():
+    metrics = str(ROOT_DIR) + "/metrics/metrics.json"
+    models = str(ROOT_DIR) + "/models/models.json"
+    with open(metrics, "r") as f:
+        metric_list = json.load(f)
+
+    with open(models, "r") as f:
+        sample_list = json.load(f)
+
+    print(sample_list)
+    dataframe = pandas.DataFrame.from_dict(metric_list)
+    dataframe2 = pandas.DataFrame.from_dict(sample_list)
+
+    data_merged = dataframe2.merge(dataframe, left_index=True, right_index=True)
+
+    csv_path = str(ROOT_DIR) + "/experiment_data.csv"
+    data_merged.to_csv(csv_path)
+
+    print(dataframe)
+    print(dataframe2)
+    print(data_merged)
+    return data_merged
+
+
+def plot_parallel_coordinates(df):
+    fig = px.parallel_coordinates(
+        df,
+        color="default",
+        color_continuous_scale=px.colors.diverging.Tealrose,
+    )
+    fig.show()
+
+
+def read_csv():
+    return pandas.read_csv(str(ROOT_DIR) + "/experiment_data_comp_sp.csv")
+
+
+if __name__ == "__main__":
+    df = read_csv()
+    print(df)
+    df.drop("")
+    plot_parallel_coordinates(df)
