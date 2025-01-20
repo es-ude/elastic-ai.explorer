8d9ab798a06819aaf12bd90beb2c7b24ab922e3e
Natalie Maman <natalie.maman@uni-due.de>
2025-01-20 22:10:04 +0100

    feat: add logging

--- mlp_for_pi.py
+++ mlp_for_pi.py
@@ -2,8 +2,9 @@ import logging
 import os
 from logging import config
 
-from scipy.stats import kendalltau
+import nni
 
+from elasticai.explorer.data_to_csv import build_search_space_measurements_file
 from elasticai.explorer.explorer import Explorer
 from elasticai.explorer.knowledge_repository import (
     KnowledgeRepository,
@@ -16,8 +17,10 @@ from elasticai.explorer.train_model import train, test
 from elasticai.explorer.visualizer import Visualizer
 from settings import ROOT_DIR
 
-logging.config.fileConfig("logging.conf")
-logger = logging.getLogger(__name__)
+nni.enable_global_logging(False)
+logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
+
+logger = logging.getLogger("explorer.main")
 
 
 def setup_knowledge_repository():
@@ -60,16 +63,15 @@ def find_generate_measure_for_pi(
         data_path = str(ROOT_DIR) + "/data"
         explorer.generate_for_hw_platform(model, model_path)
 
-        mean, std = explorer.run_latency_measurement(device_connection, model_path)
+        mean, _ = explorer.run_latency_measurement(device_connection, model_path)
         measurements_latency_mean.append(mean)
-        measurements_latency_std.append(std)
         measurements_accuracy.append(
             explorer.run_accuracy_measurement(device_connection, model_path, data_path)
         )
 
-    logger.info("Accuracy: %s", measurements_accuracy)
     floats = [float(np_float) for np_float in measurements_latency_mean]
-    logger.info("Latency in Microseconds: %s", floats)
+    df = build_search_space_measurements_file(floats)
+    logger.info("Models:\n %s", df)
 
     return Metrics(
         "metrics/metrics.json",
@@ -108,23 +110,6 @@ def prepare_pi():
     hw_manager.compile_code()
 
 
-def compute_kandell(metrics: Metrics):
-    # Taking values from the above example in Lists
-    measured_latency_rank = [
-        sorted(metrics.raw_measured_latencies).index(x)
-        for x in metrics.raw_measured_latencies
-    ]
-    est_flops_rank = [
-        sorted(metrics.structured_est_flops).index(x)
-        for x in metrics.structured_est_flops
-    ]
-
-    # Calculating Kendall Rank correlation
-    corr, _ = kendalltau(measured_latency_rank, est_flops_rank)
-    print(corr)
-    logger.info("Kendall Rank correlation: %.5f", corr)
-
-
 def make_dirs_if_not_exists():
     if not os.path.exists("plots"):
         os.makedirs("plots")
@@ -138,16 +123,15 @@ if __name__ == "__main__":
     host = "transpi5.local"
     user = "ies"
     # 60 possible
-    max_search_trials = 4
-    top_k = 3
-    measure_accuracy = 3
-    logger.info("Accuracy: %i", measure_accuracy)
+    # ca hälfte über 90%
+    # 1/3 der modelle unter 70
+    # 1/4 der Modelle unter 50
+    max_search_trials = 6
+    top_k = 2
     knowledge_repo = setup_knowledge_repository()
     device_connection = ConnectionData(host, user)
     metry = find_generate_measure_for_pi(
         knowledge_repo, device_connection, max_search_trials, top_k
     )
     visu = Visualizer(metry)
-    visu.plot_all_results(filename="ploty")
-
-    compute_kandell(metrics=metry)
+    visu.plot_all_results(filename="plot")
