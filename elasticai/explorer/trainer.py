b0eb4b3a55d39d99ab9ec3d81c88b7fe654b7321
RobinFeldmann <robinfeld.ued@gmail.com>
2025-01-30 16:58:27 +0100

    refactor: Added abstract Trainer class and MLPTrainer, class removed dependencies on train_model.py

---
+++ elasticai/explorer/trainer.py
@@ -0,0 +1,97 @@
+from abc import ABC, abstractmethod
+import logging
+from matplotlib import transforms
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+
+logger = logging.getLogger("explorer.Trainer")
+
+class Trainer(ABC):
+    @abstractmethod
+    def train(self, model: nn.Module, trainloader: DataLoader, epochs: int):
+        pass
+    
+    @abstractmethod
+    def test(self, model: nn.Module, testloader: DataLoader) -> float:
+        pass
+
+class MLPTrainer(Trainer):
+    def __init__(self, device: str,
+                  optimizer: optim, loss_fn = nn.CrossEntropyLoss()):
+
+        self.device = device
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.trainloader = None
+        self.testloader = None
+
+    def train(self, model: nn.Module, trainloader: DataLoader, epochs: int):
+        """Train the given model on the traindata
+
+        Args:
+            model (nn.Module): model
+            trainloader (DataLoader): Data
+            epochs (int): epochs
+        """
+
+        self.trainloader = trainloader
+        model.to(self.device)
+        
+        for epoch in range(epochs):
+            self._train_epoch(epoch)
+
+    def test(self, model: nn.Module, testloader: DataLoader) -> float:
+        """Test the model on the 
+
+        Args:
+            testloader (DataLoader): The data for testing.
+
+        Returns:
+            float: Accuracy on test data.
+        """
+        self.testloader = testloader
+        test_loss = 0
+        correct = 0
+        model.eval()
+        with torch.no_grad():
+            for data, target in self.testloader:
+                data, target = data.to(self.device), target.to(self.device)
+                output = model(data)
+                pred = output.argmax(dim=1, keepdim=True)
+                correct += pred.eq(target.view_as(pred)).sum().item()
+        test_loss /= len(self.testloader.dataset)
+        accuracy = 100.0 * correct / len(self.testloader.dataset)
+        logger.info(
+            "Test set: Accuracy: {}/{} ({:.0f}%)\n".format(
+                correct, len(self.testloader.dataset), accuracy
+            )
+        )
+        return accuracy
+
+    def _train_epoch(self, model: nn.Module, epoch: int):
+        """train for an epoch using
+
+        Args:
+            epoch (int): Current epoch
+        """
+        
+        model.train(True)
+        for batch_idx, (data, target) in enumerate(self.trainloader):
+            data, target = data.to(self.device), target.to(self.device)
+            self.optimizer.zero_grad()
+            output = model(data)
+            loss = self.loss_fn(output, target)
+            loss.backward()
+            self.optimizer.step()
+            if batch_idx % 10 == 0:
+                logger.debug(
+                    "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                        epoch,
+                        batch_idx * len(data),
+                        len(self.train_loader.dataset),
+                        100.0 * batch_idx / len(self.train_loader),
+                        loss.item(),
+                    )
+                )
