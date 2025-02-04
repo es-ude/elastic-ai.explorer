998d460bae45547376613155d1c90a3316ffdcee
RobinFeldmann <robinfeld.ued@gmail.com>
2025-02-04 11:04:48 +0100

    feat: add default values to config

--- elasticai/explorer/config.py
+++ elasticai/explorer/config.py
@@ -1,41 +1,44 @@
 import datetime
+import logging
 
 import yaml
 from settings import ROOT_DIR
 
+logger = logging.getLogger("explorer.config")
 class Config:
     def __init__(self, config_path: str):
 
         with open(config_path) as stream:
             try:
-                yaml_dict = yaml.safe_load(stream)
+                yaml_dict: dict = yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 print(exc)
 
-        self.experiment_conf = ExperimentConfig(yaml_dict["ExperimentConfig"])
-        self.connection_conf = ConnectionConfig(yaml_dict["ConnectionConfig"])
-        self.model_conf = ModelConfig(yaml_dict["ModelConfig"])
+        self.experiment_conf = ExperimentConfig(yaml_dict.get("ExperimentConfig", {}))
+        self.connection_conf = ConnectionConfig(yaml_dict.get("ConnectionConfig", {}))
+        self.model_conf = ModelConfig(yaml_dict.get("ModelConfig", {}))
 
 
 class ExperimentConfig:
     def __init__(self, yaml_dict: dict):
-        self.host_device: str = yaml_dict["host_device"]
-       
-        self.max_search_trials: int = yaml_dict["max_search_trials"]
-        self.top_k: int =  yaml_dict["top_k"]
-
-        try:
-            self.experiment_name = yaml_dict["experiment_name"]
-        except KeyError:
-            print("No experiment name given, timestamp is used as default.")
-            self.experiment_name = f"{datetime.datetime.now():%Y-%m-%d-%H-%M-%S}"
+        
+        #sets member variables to the values in yaml dict
+        #set to default value, if yaml dict does not define a valu
+        self.host_processor: str = yaml_dict.get("host_processor", "cpu")
+        self.max_search_trials: int = yaml_dict.get("max_search_trials", 6)
+        self.top_k: int =  yaml_dict.get("top_k", 2)
+        self.experiment_name = yaml_dict.get("experiment_name", f"{datetime.datetime.now():%Y-%m-%d-%H-%M-%S}")
         self.experiment_dir = ROOT_DIR / "experiments" / self.experiment_name
-
 
 class ConnectionConfig:
     def __init__(self, yaml_dict: dict):
-        self.target_host: str = yaml_dict["target_host"]
-        self.target_user: str = yaml_dict["target_user"]
+        try:
+            self.target_host: str = yaml_dict["target_host"]
+            self.target_user: str = yaml_dict["target_user"]
+        except KeyError:
+            logger.info("ConnectionConfig is not specified completely! Please specify or target connection is not possible.")
+            exit(-1)
+
 
 class ModelConfig:
     def __init__(self, yaml_dict: dict):
