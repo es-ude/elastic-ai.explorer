759e24a476b8d0e101082e225492b3bcc42d485d
RobinFeldmann <robinfeld.ued@gmail.com>
2025-02-07 13:09:09 +0100

    refactor(cost_estimator): delete unused function

--- elasticai/explorer/cost_estimator.py
+++ elasticai/explorer/cost_estimator.py
@@ -17,23 +17,6 @@ Wrapper for FlopsProfiler could extend in future
 
 class FlopsEstimator:
 
-    def estimate_flops_grid(self, model_space: ModelSpace):
-        """Print FLOPS-Information over the whole searchspace.
-
-        Args:
-            model_space (ModelSpace): A mutable ModelSpace
-        """
-
-        data_sample = torch.full((1, 1, 28, 28), 1.0)
-
-        profiler = FlopsProfiler(model_space, data_sample)
-
-        print("General Flops, Formular: ", profiler.expression)
-        for model_sample in model_space.grid():
-
-            profiler = FlopsProfiler(model_sample, data_sample)
-            print("Flops for", model_sample, ": ", profiler.expression)
-
     def estimate_flops_single_module(self, model_sample: torch.nn.Module) -> int:
         """Computes FLOPS for a single module.
 
