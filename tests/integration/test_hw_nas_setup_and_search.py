545c863dcd8e10084be1b45b02f172ad7df277b7
RobinFeldmann <robinfeld.ued@gmail.com>
2025-03-25 14:45:33 +0100

    feat(integration_tests): added some test coverage for explorer

--- tests/integration/test_hw_nas_setup_and_search.py
+++ tests/integration/test_hw_nas_setup_and_search.py
@@ -2,19 +2,20 @@ import os
 import unittest
 import torch
 from elasticai.explorer import search_space
+from elasticai.explorer.config import HWNASConfig
 from elasticai.explorer.explorer import Explorer
 from elasticai.explorer.knowledge_repository import HWPlatform, KnowledgeRepository
 from elasticai.explorer.platforms.deployment.manager import PIHWManager
 from elasticai.explorer.platforms.generator.generator import PIGenerator
 from torch import nn
 from settings import ROOT_DIR
-from tests.samples.sample_MLP import sample_MLP
+from tests.integration.samples.sample_MLP import sample_MLP
 
 SAMPLE_PATH = ROOT_DIR / "tests/samples"
 OUTPUT_PATH = ROOT_DIR / "tests/outputs"
 
 
-class TestHWNasSetupAndSearch(unittest.TestCase):
+class TestHWNasSetupAndSearch:
     """Integration test of the Explorer HW-NAS pipeline without a target device.
     """
     def setUp(self):
@@ -27,49 +28,35 @@ class TestHWNasSetupAndSearch(unittest.TestCase):
             PIHWManager,
         )
         )
-        self.RPI5explorer = Explorer(knowledge_rep)
-        self.ts_model_output_path = OUTPUT_PATH / "output_model_ts.pt"
-
-
+        self.RPI5explorer = Explorer(knowledge_rep,"only_for_integration_tests2")
+        self.model_name ="ts_model_0.pt"
+        self.hwnas_cfg = HWNASConfig(config_path="tests/integration/configs/hwnas_config.yaml")
     def test_search(self):
+        self.setUp()
         self.RPI5explorer.generate_search_space()
-        top_k_models = self.RPI5explorer.search(1,2)
-        self.assertEqual(len(top_k_models), 1)
-        self.assertEqual(type(top_k_models[0]), search_space.MLP)
-        
+        top_k_models = self.RPI5explorer.search(self.hwnas_cfg)
+        assert len(top_k_models) == 1
+        assert (type(top_k_models[0]) == search_space.MLP)
+        self.tearDown()
+
     def test_generate_for_hw_platform(self):
+        self.setUp()
         self.RPI5explorer.choose_target_hw("rpi5")
         model = sample_MLP()
         
-        self.RPI5explorer.generate_for_hw_platform( model= model , path = self.ts_model_output_path)
-        self.assertTrue(os.path.exists(self.ts_model_output_path))
-        self.assertTrue(torch.jit.load(self.ts_model_output_path))
-
+        self.RPI5explorer.generate_for_hw_platform(model= model , model_name = self.model_name)
+        assert os.path.exists("experiments/only_for_integration_tests2/models/" + self.model_name) == True
+        assert type(torch.jit.load("experiments/only_for_integration_tests2/models/" + self.model_name)) == torch.jit._script.RecursiveScriptModule
+        self.tearDown()
+        
     def test_set_default_model(self):
+        self.setUp()
         model = sample_MLP()
         self.RPI5explorer.set_default_model(model)
-        self.assertEquals(self.RPI5explorer.default_model, model)
+        assert self.RPI5explorer.default_model == model 
+        self.tearDown()
 
     def tearDown(self):
         del self.RPI5explorer
-        if os.path.exists(self.ts_model_output_path):
-            os.remove(self.ts_model_output_path)
-    
-def only_test_search():
-    suite = unittest.TestSuite()
-    suite.addTest(TestHWNasSetupAndSearch('test_search'))
-    return suite
-def only_test_generate_for_hw_platform():
-    suite = unittest.TestSuite()
-    suite.addTest(TestHWNasSetupAndSearch('test_generate_for_hw_platform'))
-    return suite
-def only_test_set_default_model():
-    suite = unittest.TestSuite()
-    suite.addTest(TestHWNasSetupAndSearch('test_set_default_model'))
-    return suite
-
-
-if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
-    unittest.main()
-    
+        if os.path.exists("experiments/only_for_integration_tests2/models/" + self.model_name):
+            os.remove("experiments/only_for_integration_tests2/models/" + self.model_name)
