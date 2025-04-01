7ee06e8c34fb45c784260bfd3b7c129c5a003f14
Natalie Maman <natalie.maman@uni-due.de>
2025-04-01 17:21:08 +0200

    test: add accuracy test

--- tests/unit_tests/test_manager.py
+++ tests/unit_tests/test_manager.py
@@ -14,30 +14,6 @@ class TestPiHWManager(unittest.TestCase):
         command = builder.build()
         self.assertEqual("./measure_latency model_0.pt dataset", command)
 
-    # def test_run_latency_measurements(self):
-    #     target = MagicMock()
-    #     compiler = Mock()
-    #     expected = "Latency in us: 574747"
-    #     attr = {"run_command.return_value": expected}
-    #     target.configure_mock(**attr)
-    #     self.hwmanager = PIHWManager(target, compiler)
-    #     path: Path = Path(str(CONTEXT_PATH)) / "bin" / "measure_latency"
-    #
-    #     result = self.hwmanager.measure_latency(path_to_model=path)
-    #     self.assertEqual(("Latency in us", "574747"), result)
-
-    # def test_run_accuracy_measurements(self):
-    #     target = MagicMock()
-    #     compiler = Mock()
-    #     expected = "Accuracy: 94.5"
-    #     attr = {"run_command.return_value": expected}
-    #     target.configure_mock(**attr)
-    #     self.hwmanager = PIHWManager(target, compiler)
-    #     path: Path = Path(str(CONTEXT_PATH)) / "bin" / "measure_accuracy"
-    #
-    #     result = self.hwmanager.measure_metric("accuracy", path_to_model=path, path_to_data="/data")
-    #     self.assertEqual(("Accuracy", "94.5"), result)
-
     def test_run_latency_measurements(self):
         target = MagicMock()
         compiler = Mock()
@@ -57,19 +33,25 @@ class TestPiHWManager(unittest.TestCase):
         result = self.hwmanager.measure_metric(metric, path_to_model=path, path_to_data=None)
         self.assertEqual(expected, result)
 
-    # def test_read_json_metric(self):
-    #     expected = {
-    #         "latency": {
-    #             "value": 57474,
-    #             "unit": "microseconds"
-    #         }
-    #     }
-    #     target = MagicMock()
-    #     attr = {"run_command.return_value": expected}
-    #     target.configure_mock(**attr)
-    #     compiler = Mock()
-    #     self.hwmanager= PIHWManager(target, compiler)
-    #
+    def test_run_accuracy_measurements(self):
+        target = MagicMock()
+        compiler = Mock()
+        output = '{ "Accuracy": { "value": 94.8 , "unit": "percent"}}'
+
+        expected = {
+            "Accuracy": {
+                "value": 94.8,
+                "unit": "percent"
+            }
+        }
+        attr = {"run_command.return_value": output}
+        target.configure_mock(**attr)
+        self.hwmanager = PIHWManager(target, compiler)
+        path: Path = Path(str(CONTEXT_PATH)) / "bin" / "measure_accuracy"
+        datapath: Path = Path(str(CONTEXT_PATH)) / "data" / "mnist.zip"
+        metric = Metric.ACCURACY
+        result = self.hwmanager.measure_metric(metric, path_to_model=path, path_to_data=datapath)
+        self.assertEqual(expected, result)
 
 
 if __name__ == '__main__':
