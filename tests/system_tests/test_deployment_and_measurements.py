7b4033d5e19e5f2501f6f0c4607cb39aa8beafb3
RobinFeldmann <robinfeld.ued@gmail.com>
2025-04-11 14:35:40 +0200

    fix(system_tests):  update to new explorer interface

--- tests/system_tests/test_deployment_and_measurements.py
+++ tests/system_tests/test_deployment_and_measurements.py
@@ -1,3 +1,4 @@
+import shutil
 from elasticai.explorer.config import HWNASConfig, ModelConfig, DeploymentConfig
 from elasticai.explorer.explorer import Explorer
 from elasticai.explorer.knowledge_repository import HWPlatform, KnowledgeRepository
@@ -5,6 +6,9 @@ from elasticai.explorer.platforms.deployment.compiler import Compiler
 from elasticai.explorer.platforms.deployment.manager import PIHWManager, Metric
 from elasticai.explorer.platforms.generator.generator import PIGenerator
 from elasticai.explorer.platforms.deployment.device_communication import Host
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from torchvision.transforms import transforms
 from pathlib import Path
 
 
@@ -35,7 +39,18 @@ class TestDeploymentAndMeasurement:
         self.RPI5explorer._model_dir = Path("tests/system_tests/samples")
         self.RPI5explorer.choose_target_hw(self.deploy_cfg)
         self.model_name = "ts_model_0.pt"
-        self.RPI5explorer.hw_setup_on_target()
+        transf = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+        )
+        path_to_dataset = Path("tests/system_tests/samples/data/mnist")
+        __trainloader: DataLoader = DataLoader(
+            MNIST(path_to_dataset, download=True, transform=transf),
+            batch_size=64,
+            shuffle=True,
+        )
+        path_to_data_docker = "docker/data/mnist"
+        shutil.make_archive(path_to_data_docker, "zip", path_to_dataset)
+        self.RPI5explorer.hw_setup_on_target(Path(path_to_data_docker + ".zip"))
 
     def test_run_accuracy_measurement(self):
         self.setUp()
@@ -43,8 +58,7 @@ class TestDeploymentAndMeasurement:
             type(
                 self.RPI5explorer.run_measurement(
                     Metric.ACCURACY,
-                    model_name=self.model_name,
-                    path_to_data=Path("docker/data"),
+                    model_name=self.model_name
                 )["Accuracy"]["value"]
             )
             == float
@@ -57,7 +71,6 @@ class TestDeploymentAndMeasurement:
                 self.RPI5explorer.run_measurement(
                     Metric.LATENCY,
                     model_name=self.model_name,
-                    path_to_data=Path("docker/data"),
                 )["Latency"]["value"]
             )
             == int
