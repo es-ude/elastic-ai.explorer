738d626ac9bba0a3911cd28ba7ec4b1471bb5024
Natalie Maman <natalie.maman@uni-due.de>
2025-07-11 15:44:13 +0200

    feat: conv2d mlp sp working

--- elasticai/explorer/hw_nas/search_space/construct_sp.py
+++ elasticai/explorer/hw_nas/search_space/construct_sp.py
@@ -611,40 +611,46 @@ class OptunaSearchSpace:
                                                      search_params["out_channels"])
             kernel_size = trial.suggest_categorical("kernel_size_b{}_l{}".format(block_id, i),
                                                     search_params["kernel_size"])
-            stride = trial.suggest_categorical("stride_l{}".format(block, i),
+            stride = trial.suggest_categorical("stride_b{}_l{}".format(block_id, i),
                                                search_params["stride"])
             activation = trial.suggest_categorical("activation_func_b{}_l{}".format(block_id, i), block["activation"])
             self.layers.append(nn.Conv2d(self.input_shape[0], out_channels, kernel_size, stride))
             self.layers.append(activation_mapping[activation])
             self.input_shape = calculate_conv_output_shape(self.input_shape,out_channels,kernel_size,stride)
+
     def create_block(self, trial, block: dict):
         operation_candidates=block["op_candidates"]
-
-        num_layers = trial.suggest_categorical("depth_b{}".format(block["block"]), block["depth"])
-        match operation_candidates:
+        print(operation_candidates)
+        num_layers = trial.suggest_categorical("num_layers_b{}".format(block["block"]), block["depth"])
+        operation= trial.suggest_categorical("operation_b{}".format(block["block"]), operation_candidates)
+        match operation:
             case "linear":
                 self.createLinear(trial, block,num_layers,block["linear"])
+                print("linear")
             case "conv2d":
-                self.createConv2d(trial, block, num_layers, block["linear"])
+                self.createConv2d(trial, block, num_layers, block["conv2D"])
+                print("conv")
 
 
 
     def create_model_sample(self, trial):
         for block in self.blocks:
+
             self.create_block(trial, block)
-
-
+        return nn.Sequential(*self.layers)
 
 search_space= yml_to_dict("search_space.yml")
 search_space= OptunaSearchSpace(search_space)
 
 def objective(trial):
+    search_space = yml_to_dict("search_space.yml")
+    search_space = OptunaSearchSpace(search_space)
     return search_space.create_model_sample(trial)
 
 if __name__ == "__main__":
     search_space= yml_to_dict("search_space.yml")
     search_space=OptunaSearchSpace(search_space)
-    sample={"num_layers": 2,"layer_op_l1":"linear","layer_op_l0":"conv2d","layer_width_l1": 128,"out_channels_l0": 16,"stride_l0": 1,"kernel_size_l0": 2, "activation_func_l0": "relu", "activation_func_l1": "sigmoid"  }
+    sample={"num_layers_b1": 2,"num_layers_b2": 1, "operation_b1":"conv2d","operation_b2":"linear","layer_width_b2_l0": 21,"out_channels_b1_l0": 4,"out_channels_b1_l1": 10,"stride_b1_l0": 1,"stride_b1_l1": 1,"kernel_size_b1_l0": 2, "kernel_size_b1_l1": 2,"activation_func_b1_l0": "relu","activation_func_b1_l1": "relu" ,"activation_func_b2_l0": "sigmoid"  }
   #  sample={"num_layers": 2,"layer_op_l1":"linear","layer_op_l0":"conv2d","layer_width_l1": 128,"out_channels_l0": 16,"stride_l0": 1,"kernel_size_l0": 2, "activation_func_l0": "relu", "activation_func_l1": "sigmoid" }
     model=objective(FixedTrial(sample))
     print(model)
