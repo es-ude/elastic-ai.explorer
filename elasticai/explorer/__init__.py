507d3eba0a45fa784ea89f09155366e176a932c4
Andreas Erbslöh <andreas.erbsloeh@uni-due.de>
2026-07-08 11:33:00 +0200

    feat(main): Add get_path_to_project func

---
+++ elasticai/explorer/__init__.py
@@ -0,0 +1,1 @@
+from ._helper import get_path_to_project as get_path_to_project
---
+++ elasticai/explorer/_helper.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+
+def get_path_to_project(new_folder: str = "") -> Path:
+    """Function for getting the path to find the project folder structure in application.
+    :return:            Absolute path to start the project structure
+    """
+    max_levels: int = 5
+
+    cwd = Path(".").absolute()
+    current = cwd
+
+    for _ in range(max_levels):
+        if (current / "pyproject.toml").exists():
+            break
+        current = current.parent
+    return (current / new_folder).resolve()
