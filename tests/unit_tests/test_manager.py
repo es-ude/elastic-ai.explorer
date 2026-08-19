4b6103c4a3a9a14f112e265321c9093d6512030b
dominik <dominik.sukic@wwb.de>
2026-08-19 22:51:35 +0200

    Replaced DOCKER_CONTEXT_DIR with RBPI_CROSSCOMPILE_DIR

--- tests/unit_tests/test_manager.py
+++ tests/unit_tests/test_manager.py
@@ -4,9 +4,9 @@ from unittest.mock import MagicMock, Mock
 from elasticai.explorer.platforms.deployment.hw_manager import (
     CommandBuilder,
     RPiHWManager,
-    DOCKER_CONTEXT_DIR,
     Metric,
 )
+from settings import RBPI_CROSSCOMPILE_DIR
 
 
 class TestPiHWManager:
@@ -27,7 +27,7 @@ class TestPiHWManager:
         attr = {"run_command.return_value": output}
         target.configure_mock(**attr)
         self.hwmanager = RPiHWManager(target, compiler)
-        path: Path = Path(str(DOCKER_CONTEXT_DIR)) / "bin" / "measure_latency"
+        path: Path = Path(str(RBPI_CROSSCOMPILE_DIR)) / "bin" / "measure_latency"
         self.hwmanager._register_metric_to_source(
             Metric.LATENCY, Path("measure_latency")
         )
@@ -48,7 +48,7 @@ class TestPiHWManager:
         self.hwmanager._register_metric_to_source(
             Metric.ACCURACY, Path("measure_accuracy")
         )
-        path: Path = Path(str(DOCKER_CONTEXT_DIR)) / "bin" / "measure_accuracy"
+        path: Path = Path(str(RBPI_CROSSCOMPILE_DIR)) / "bin" / "measure_accuracy"
         metric = Metric.ACCURACY
         result = self.hwmanager.measure_metric(metric, path_to_model=path)
         assert expected == result
