be6f9b9d6b5fe3a4ceb57d2e026680375ee0894c
dominik <dominik.sukic@wwb.de>
2026-08-19 22:50:31 +0200

    Removed DOCKER_CONTEXT_DIR and replaced it with crosscompile environments dirs

--- settings.py
+++ settings.py
@@ -2,5 +2,7 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).resolve().parent
 MAIN_EXPERIMENT_DIR = ROOT_DIR / "experiments"
-DOCKER_CONTEXT_DIR = ROOT_DIR / "docker"
+CROSSCOMPILE_ENV_DIR = ROOT_DIR / "crosscompile-environments"
+RBPI_CROSSCOMPILE_DIR = CROSSCOMPILE_ENV_DIR / "rbpi"
+PICO_CROSSCOMPILE_DIR = CROSSCOMPILE_ENV_DIR / "pico"
 
